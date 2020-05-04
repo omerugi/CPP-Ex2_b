799b66494119ca6bb067c5cf7cf73e30e7c5d97c
omerugi <omerihay@gmail.com>
2020-05-04 11:21:49 +0300

    fix

--- solver.cpp
+++ solver.cpp
@@ -1,5 +1,265 @@
-//
-// Created by omerugi on 25/04/2020.
-//
-
+#include <iostream>
+#include <complex>
 #include "solver.hpp"
+
+using namespace std;
+using namespace solver;
+
+
+RealVariable& solver::operator^(RealVariable &x, int q) {
+
+    if(q == 1){
+        return x;
+    }
+    if(q == 0){
+        auto *x1 = new solver::RealVariable;
+        x1->b = 0;
+        x1->c++;
+        return *(x1);
+    }
+    if (q>2){
+        throw runtime_error("power greater then 2");
+    }
+
+    auto *x1 = new solver::RealVariable;
+    x1->b = 0;
+    x1->a++;
+
+    return *(x1);
+}
+
+RealVariable& solver::operator==(RealVariable &x, int a) {
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+}
+
+RealVariable &solver::operator==(RealVariable &x, double a) {
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+
+}
+
+RealVariable &solver::operator==(RealVariable &x, RealVariable &y) {
+
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->a -= y.a;
+    x1->b -= y.b;
+    x1->c -= y.c;
+
+    return *(x1);
+
+}
+
+RealVariable &solver::operator*(int a, RealVariable &x) {
+
+    auto *x1 = new solver::RealVariable(x);
+
+    x1->a *=a;
+    x1->b *= a;
+    x1->c *= a;
+    return *(x1);
+}
+
+RealVariable &solver::operator-(RealVariable &x, int a) {
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->c -= a;
+    return *(x1);
+}
+
+RealVariable &solver::operator-(RealVariable &x, RealVariable &y) {
+
+    RealVariable *x1;
+    if(x.is_zero()){
+        x1 = new solver::RealVariable(x);
+        x1->b = 1;
+    }
+    else{
+        x1 = new solver::RealVariable(x);
+    }
+    RealVariable *y1;
+    if(y.is_zero()){
+        y1 = new solver::RealVariable(y);
+        y1->b =1;
+    }
+    else{
+        y1 = new solver::RealVariable(y);
+    }
+    x1->a -= y1->a;
+    x1->b -= y1->b;
+    x1->c -= y1->c;
+
+    return *(x1);
+}
+
+RealVariable &solver::operator+(RealVariable &x, int a) {
+
+    auto *x1 = new solver::RealVariable(x);
+
+
+    x1->c += a;
+    return *(x1);
+}
+
+RealVariable &solver::operator+(RealVariable &x, RealVariable &y) {
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->a += y.a;
+    x1->b += y.b;
+    x1->c += y.c;
+
+    return *(x1);
+}
+
+RealVariable &solver::operator+(int a, RealVariable &x) {
+
+    auto *x1 = new solver::RealVariable(x);
+    x1->c += a;
+    return *(x1);
+}
+
+RealVariable &solver::operator/(RealVariable &x, int a) {
+
+    if(a == 0){
+        throw runtime_error ("divied by zero");
+    }
+    auto *x1 = new solver::RealVariable(x);
+    x1->b /= a;
+    return *(x1);
+
+}
+
+bool RealVariable::is_zero(){
+
+    return a == 0 && b == 0 && c == 0 ;
+
+}
+
+ComplexVariable solver::operator*(int a, ComplexVariable y) {
+    auto *x1 = new solver::ComplexVariable(y);
+
+    x1->a *=a;
+    x1->b *= a;
+    x1->c *= a;
+    return *(x1);
+}
+
+ComplexVariable solver::operator-(ComplexVariable x, int a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c -= a;
+    return *(x1);
+}
+
+ComplexVariable solver::operator-(ComplexVariable x, ComplexVariable y){
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->a -= y.a;
+    x1->b -= y.b;
+    x1->c -= y.c;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator-(ComplexVariable x, complex<double> a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator==(ComplexVariable x, int a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator==(ComplexVariable x, double a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator==(ComplexVariable x, ComplexVariable y) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->a -= y.a;
+    x1->b -= y.b;
+    x1->c -= y.c;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator==(ComplexVariable x, complex<double> a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c-=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator^(ComplexVariable x, int q) {
+    if(q == 1){
+        return x;
+    }
+    if(q == 0){
+        auto *x1 = new solver::ComplexVariable;
+        x1->b = 0;
+        x1->c+=1;
+        return *(x1);
+    }
+    if (q>2||q<0){
+        throw runtime_error("power greater then 2");
+    }
+
+    auto *x1 = new solver::ComplexVariable;
+    x1->b = 0;
+    x1->a++;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator+(ComplexVariable x, ComplexVariable y) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->a += y.a;
+    x1->b += y.b;
+    x1->c += y.c;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator+(ComplexVariable x, int a) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c+=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator+(int a, ComplexVariable x) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c+=a;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator+(ComplexVariable x, complex<double> v) {
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->c+=v;
+
+    return *(x1);
+}
+
+ComplexVariable solver::operator/(ComplexVariable x, int a) {
+    if(a == 0){
+        throw runtime_error ("divied by zero");
+    }
+    auto *x1 = new solver::ComplexVariable(x);
+    x1->b /= a;
+    return *(x1);
+}
+
--- solver.hpp
+++ solver.hpp
@@ -6,6 +6,7 @@
 #define UNTITLED5_SOLVER_HPP
 #include <iostream>
 #include <complex>
+#include <cmath>
 
 using namespace std;
 
@@ -14,54 +15,95 @@ namespace solver{
     class RealVariable{
 
     public:
-        RealVariable()= default;
-        RealVariable operator ^ (int a){
-            RealVariable x;
-            return x; }
-        friend RealVariable operator == (RealVariable x, int a){ return x;};
-        friend RealVariable operator == (RealVariable x, double a){ return x;};
-        friend RealVariable operator == (RealVariable x, RealVariable y){  return x;};
-        friend RealVariable operator * (int a,solver::RealVariable x){  return x;}
-        friend RealVariable operator - (solver::RealVariable x,int a){  return x;}
-        friend RealVariable operator - (solver::RealVariable x,solver::RealVariable y){  return x;}
-        friend RealVariable operator + (solver::RealVariable x,int a){  return x;}
-        friend RealVariable operator + (solver::RealVariable x,solver::RealVariable y){  return x;}
-        friend RealVariable operator + (int a,solver::RealVariable x){  return x;}
-        friend RealVariable operator / (solver::RealVariable x,int a){  return x;}
+        double a;
+        double b;
+        double c;
+        RealVariable():a(0),b(1),c(0){};
+
+        RealVariable(RealVariable &x1):a(x1.a),b(x1.b),c(x1.c){}
+
+        friend RealVariable& operator^(RealVariable& x,int q);
+        friend RealVariable& operator == (RealVariable& x, int a);
+        friend RealVariable& operator == (RealVariable& x, double a);
+        friend RealVariable& operator == (RealVariable& x, RealVariable& y);
+        friend RealVariable& operator * (int a,RealVariable& x);
+        friend RealVariable& operator - (RealVariable& x,int a);
+        friend RealVariable& operator - (RealVariable& x,RealVariable& y);
+        friend RealVariable& operator + (RealVariable& x,int a);
+        friend RealVariable& operator + (RealVariable& x,RealVariable& y);
+        friend RealVariable& operator + (int a,RealVariable& x);
+        friend RealVariable& operator / (RealVariable& x,int a);
+
+        bool is_zero();
     };
 
     class ComplexVariable{
+
     public:
-        ComplexVariable ()= default;
-        friend ComplexVariable operator * (int a, ComplexVariable y ){ return y;}
-        friend ComplexVariable operator -(ComplexVariable y , int a){return y;}
-        friend ComplexVariable operator - (ComplexVariable y1, ComplexVariable y2){return y1;}
-        friend ComplexVariable operator - (ComplexVariable y, complex<double> a){return y;}
-        friend ComplexVariable operator == (ComplexVariable y , int a){return y;}
-        friend ComplexVariable operator == (ComplexVariable y , double a){return y;}
-        friend ComplexVariable operator == (ComplexVariable y1, ComplexVariable y2){return y1;}
-        friend ComplexVariable operator == (ComplexVariable y1, complex<double> a){return y1;}
-        friend ComplexVariable operator ^ (ComplexVariable y, int a){return y;}
-        friend ComplexVariable operator + (ComplexVariable y1, ComplexVariable y2){return y1;}
-        friend ComplexVariable operator + (ComplexVariable y, int a){return y;}
-        friend ComplexVariable operator + (int a, ComplexVariable y){return y;}
-        friend ComplexVariable operator + (ComplexVariable y, complex<double> v){return y;}
-        friend ComplexVariable operator / (ComplexVariable y, int a){return y;}
+        double a;
+        complex<double> b;
+        complex<double> c;
+        ComplexVariable():a(0),b(1),c(0){};
+
+        friend ComplexVariable operator * (int a, ComplexVariable y );
+        friend ComplexVariable operator -(ComplexVariable y , int a);
+        friend ComplexVariable operator - (ComplexVariable y1, ComplexVariable y2);
+        friend ComplexVariable operator - (ComplexVariable y, complex<double> a)  ;
+        friend ComplexVariable operator == (ComplexVariable y , int a)   ;
+        friend ComplexVariable operator == (ComplexVariable y , double a);
+        friend ComplexVariable operator == (ComplexVariable y1, ComplexVariable y2);
+        friend ComplexVariable operator == (ComplexVariable y1, complex<double> a) ;
+        friend ComplexVariable operator ^ (ComplexVariable y, int a);
+        friend ComplexVariable operator + (ComplexVariable y1, ComplexVariable y2);
+        friend ComplexVariable operator + (ComplexVariable y, int a);
+        friend ComplexVariable operator + (int a, ComplexVariable y);
+        friend ComplexVariable operator + (ComplexVariable y, complex<double> v);
+        friend ComplexVariable operator / (ComplexVariable y, int a);
 
 
     };
 
-    inline double solve(RealVariable x){ return 8.0;}
-    inline std::complex<double> solve(ComplexVariable y){ return (8i);}
+    inline double solve(RealVariable x){
 
+        if(x.a == 0){
+            double res = -x.c/x.b;
+
+            if(x.b == 0){
+                throw runtime_error ("inf");
+            }
+            return res;
+        }
+
+        double delta = pow(x.b,2)-4*x.a*x.c;
+        if(delta < 0){
+            throw runtime_error ("exception: no real solution");
+        }
+        double a = 2*x.a;
+        double res = (-1*x.b + pow(delta,0.5))/a;
+
+        return res;
+    }
+
+    inline std::complex<double> solve(ComplexVariable x){
+
+        if(x.a == 0){
+            complex<double> res = -x.c/x.b;
+
+            if(x.b == complex<double>(0)){
+                throw runtime_error ("inf");
+            }
+            return res;
+        }
+
+        complex<double> delta = pow(x.b,2)-4*x.a*x.c;
+        double a = 2*x.a;
+        complex<double> res = (complex<double>(-1)*x.b + sqrt(delta))/a;
+
+        return res;
+    }
 
 }
 
-
-
-
-
-
-
+void print();
 #endif //UNTITLED5_SOLVER_HPP
 
